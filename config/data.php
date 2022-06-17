fe6a638fb1c7c047f2819e4aa1b4caa25b689833
MATIAS GIDDINGS <matias.giddings@uc.cl>
2022-06-17 19:38:08 -0400

    Connection

---
+++ config/data.php
@@ -0,0 +1,10 @@
+<?php
+    // Base 1
+    $user = 'grupo40';
+    $password = 'matichuma';
+    $db_name = 'grupo40e3';
+    // Base 2
+    $user2 = 'grupo69';
+    $password2 = 'xxxx';
+    $db_name2 = 'grupo69e3';
+?>
