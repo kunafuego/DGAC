5d77a9dcba2ae069473a198dae65bd33c27ad29c
Domingo Agüero Ureta <80777344+kunafuego@users.noreply.github.com>
2022-07-04 00:02:34 -0400

    Delete ayudantia directory

--- ayudantia/Sites/config/data.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Variables para iniciar sesión
-    $user = 'grupo998';
-    $password = 'grupo998';
-    $db_name = 'grupo998e3';
-?>
--- ayudantia/Sites/views/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['timeout']);
-    unset($_SESSION['password']);
-    unset($_SESSION['username']);
-    $_SESSION['valid'] = false;
-    header('Refresh: 0; url = ../index.php')
-?>
